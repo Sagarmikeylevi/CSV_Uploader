630b5692785a0715bb1000faef06f35e57afeadd
unknown <sagarmikeylevi@gmail.com>
2023-02-19 17:49:38 +0530

    commpleted comments part

--- controllers/csvFileController.js
+++ controllers/csvFileController.js
@@ -4,18 +4,21 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const csvFiles = [];
 
+// Create a disk storage engine for Multer
 const storage = multer.diskStorage({
+  // Set the destination directory for uploaded files
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, '../', '/uploads'));
   },
+  // Generate a unique filename for uploaded files
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.originalname + '-' + uniqueSuffix)
   }
 });
 
+// Filter out non-CSV files
 function fileFilter(req, file, cb) {
-
   if (file.mimetype == 'text/csv') {
     cb(null, true);
   }
@@ -25,15 +28,16 @@ function fileFilter(req, file, cb) {
   }
 }
 
+// Create a Multer instance with the disk storage engine and file filter
 const csvupload = multer({ storage: storage, fileFilter: fileFilter }).single('csv_file');
 
+// Handle the file upload and add the uploaded file's name to csvFiles array
 module.exports.upload = (req, res) => {
   csvupload(req, res, function (err) {
     if (err) {
       console.log("****Multer error", err);
       return;
     }
-    
     else if (req.file) {
       csvFiles.push(req.file.filename);
     }
@@ -45,11 +49,10 @@ module.exports.uploadedcsvNames = ()=>{
   return csvFiles;
 }
 
-
+// Get the parsed data of a CSV file with a given index in csvFiles array
 module.exports.open = (req , res) =>{
   const csvParsedData = [];             
   const index = req.params.index;
-  // console.log(csvFiles[index])
   fs.createReadStream(path.join(__dirname,'../','/uploads',csvFiles[index])) 
   .pipe(csv())
   .on('data', (data) => csvParsedData.push(data))
@@ -61,6 +64,7 @@ module.exports.open = (req , res) =>{
   });
 }
 
+// Delete a CSV file with a given index in csvFiles array
 module.exports.delete = function(req,res){
   let index = req.params.index;
   try { var files = fs.readdirSync(path.join(__dirname,'..','/uploads')); }
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,11 @@
 const csvFileController = require('./csvFileController');
 
+// Get the function to retrieve the names of uploaded CSV files from csvFileController
 const uploadedcsvFileNames = csvFileController.uploadedcsvNames;
-const filesNames = uploadedcsvFileNames(); 
+// Invoke the function to get the current names of uploaded CSV files
+const filesNames = uploadedcsvFileNames();
 
+// Render the home page with the current names of uploaded CSV files
 module.exports.home = (req , res) =>{
     return res.render('home' , {
         title: "HOME | CSV UPLOADER",
